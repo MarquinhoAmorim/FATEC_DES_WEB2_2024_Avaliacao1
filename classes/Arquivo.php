77cc949362f7e19dee570945d839c68d80555902
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2024-04-22 20:20:23 -0300

    Classe Arquivo - método gravar_linha

--- classes/Arquivo.php
+++ classes/Arquivo.php
@@ -1,14 +1,21 @@
 <?php 
 class Arquivo { 
 	private $arquivo; 
+    private $handle;
 	 
 	public function __construct($nome_arquivo) { 
         $this->arquivo = $nome_arquivo; 
-        print('Chamado o construtor   ');
+        $this->handle = fopen($nome_arquivo, "a+");
+        
 	} 
-	 
+
+    public function gravar($valor_da_linha) { 
+        fwrite($this->handle, $valor_da_linha);
+        fwrite($this->handle,"\n");
+	} 
+
 	public function __destruct() { 
-	    print('Chamado o destrutor   ');
+        fclose($this->handle);
 	} 
 } 
 
--- classes/arquivo_objetos.php
+++ classes/arquivo_objetos.php
@@ -1,6 +1,8 @@
 <?php
 include('Arquivo.php');
 $arquivo = new Arquivo('carro.txt'); 
+$arquivo->gravar('oi mundo');
+$arquivo->gravar('oi mundo 2');
 unset($arquivo); 
 
 ?>
