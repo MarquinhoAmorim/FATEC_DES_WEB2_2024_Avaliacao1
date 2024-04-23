9192271cc0d9e0524ca7b056dc2945744a8e0c89
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2024-04-22 22:04:37 -0300

    Atualização Classes Arquivo

--- classes/Arquivo.php
+++ classes/Arquivo.php
@@ -1,5 +1,5 @@
 <?php 
-class Arquivo { 
+class ArquivoGravar { 
 	private $arquivo; 
     private $handle;
 	 
@@ -9,7 +9,7 @@ class Arquivo {
         
 	} 
 
-    public function gravar($valor_da_linha) { 
+    public function gravar_linha($valor_da_linha) { 
         fwrite($this->handle, $valor_da_linha);
         fwrite($this->handle,"\n");
 	} 
@@ -19,4 +19,27 @@ class Arquivo {
 	} 
 } 
 
+class ArquivoLer { 
+	private $arquivo; 
+    private $handle;
+	 
+	public function __construct($nome_arquivo) { 
+        $this->arquivo = $nome_arquivo; 
+        $this->handle = fopen($nome_arquivo, "r");
+        
+	} 
+
+    public function ler_linha() { 
+        $buffer = stream_get_line($this->handle, 1096, "\n");
+        return $buffer;   
+	} 
+
+    public function eof() {
+        return feof($this->handle);
+    }
+
+	public function __destruct() { 
+        fclose($this->handle);
+	} 
+}
 ?>
--- classes/arquivo_objetos.php
+++ classes/arquivo_objetos.php
@@ -1,8 +1,16 @@
 <?php
 include('Arquivo.php');
-$arquivo = new Arquivo('carro.txt'); 
-$arquivo->gravar('oi mundo');
-$arquivo->gravar('oi mundo 2');
+
+$arquivo = new ArquivoGravar('carro.txt'); 
+$arquivo->gravar_linha('oi mundo');
+$arquivo->gravar_linha('oi mundo 2');
 unset($arquivo); 
+
+$arquivo2 = new ArquivoLer('carro.txt'); 
+while (! $arquivo2->eof()){
+    print($arquivo2->ler_linha());
+    print('<br>');
+}
+unset($arquivo2); 
 
 ?>
